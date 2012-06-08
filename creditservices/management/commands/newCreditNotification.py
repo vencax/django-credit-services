dbe438427b4cb21ca18951b677111dacdb696dbe
Vaclav Klecanda <vencax77@gmail.com>
2012-06-08 09:26:54 +0200

    small changes

--- creditservices/management/commands/newCreditNotification.py
+++ creditservices/management/commands/newCreditNotification.py
@@ -3,7 +3,6 @@ Created on Dec 29, 2011
 
 @author: vencax
 '''
-from django.core.management.base import BaseCommand
 import logging
 from django.conf import settings
 from django.contrib.auth.models import User
@@ -11,34 +10,42 @@ from valueladder.models import Thing
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext
 from django.contrib.sites.models import Site
+from mailserver.command import EmailHandlingCommand
+from invoices.models import CompanyInfo
 
-class Command(BaseCommand):
-    args = ''
+class Command(EmailHandlingCommand):
     help = 'parses incoming mail and takes actions base on it'
     
-    def handle(self, *args, **options):
-        logging.basicConfig()
-        _, _, data = args
+    def processMail(self, recipient, mailfrom, data):
         
-        pMod = __import__(settings.CREDIT_NOTIFICATION_PARSER, globals={}, locals={})
+        logging.info('Loading %s' % settings.CREDIT_NOTIFICATION_PARSER)
+        
+        pMod = __import__(settings.CREDIT_NOTIFICATION_PARSER, 
+                          globals={}, locals={}, fromlist=['Parser'])
         parser = pMod.Parser()
-        vs, _, amount, _, currencyCode = parser.parse(data)
+        parsed = parser.parse(data)
+        
+        logging.info('Parsed: %s' % str(parsed))
+        
+        vs, _, amount, _, currencyCode = parsed
         
         currency = Thing.objects.get(code=currencyCode)
         try:
             self._processParsed(vs, amount, currency)
         except User.DoesNotExist:
             self._onBadVS(data)
+        except CompanyInfo.DoesNotExist:
+            self._onBadVS(data)
             
     def _processParsed(self, vs, amount, currency):
         u = User.objects.get(pk=int(vs))
-        companyInfo = u.companyinfo
+        companyInfo = CompanyInfo.objects.get(user__id=u.id)
         try:
             creditInfo = companyInfo.credits.get(currency=currency)
         except companyInfo.credits.model.DoesNotExist:
             creditInfo = companyInfo.credits.model(company=companyInfo, value=0,
                                                    currency=currency)
-            
+        
         creditInfo.value += amount
         creditInfo.save()
         
@@ -49,8 +56,7 @@ class Command(BaseCommand):
                 'state' : creditInfo.value,
                 'domain' : Site.objects.get_current()
             })
-            u.email_user(ugettext('credit increased'), mailContent)
+            u.email_user(ugettext('credit increased'), mailContent)       
         
     def _onBadVS(self, data):
         pass
-        
