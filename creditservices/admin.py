1891d32844498e0cff33e8946a3ef59862b9bf4c
Vaclav Klecanda <vencax77@gmail.com>
2012-06-25 11:01:29 +0200

    admin for credit info

--- creditservices/admin.py
+++ creditservices/admin.py
@@ -4,12 +4,13 @@ Created on Jun 7, 2012
 @author: vencax
 '''
 from django.contrib import admin
-#from invoices.admin import CompanyInfoAdmin
 
 from .models import CreditInfo
 
-class CreditInfoAdmin(admin.TabularInline):
-    model = CreditInfo
-    extra = 1
+class CreditInfoAdmin(admin.ModelAdmin):
+    list_display = ('company', 'value', 'currency')
+    list_filter = ('currency', )
+    search_fields = ('company', 'value')
+    readonly_fields = ('company', 'currency')
     
-#CompanyInfoAdmin.inlines.append(CreditInfoAdmin)
+admin.site.register(CreditInfo, CreditInfoAdmin)
