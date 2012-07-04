6665d27d5f6278b30d862bde803b64162d574c98
Vaclav Klecanda <vencax77@gmail.com>
2012-07-04 18:29:47 +0200

    setCredit command

---
+++ creditservices/management/commands/setCredit.py
@@ -0,0 +1,32 @@
+'''
+Created on Dec 29, 2011
+
+@author: vencax
+'''
+import logging
+from invoices.models import CompanyInfo, Invoice
+from django.core.management.base import BaseCommand
+from valueladder.models import Thing
+from creditservices.signals import processCredit
+from optparse import make_option
+
+class Command(BaseCommand):
+    help = 'check credits of companies if they are not in debt too long' #@ReservedAssignment
+    
+    option_list = BaseCommand.option_list + (
+        make_option('--user', help='ID of user that the credit has to be set to'),
+        make_option('--value', help='new credit value'),
+        make_option('--currency', help='currency code'),
+    )
+    
+    def handle(self, *args, **options):
+        logging.basicConfig()
+        companyInfo = CompanyInfo.objects.get(user__id=options['user'])
+        if options['currency']:
+            currency = Thing.objects.get(code=options['currency'])
+        else:
+            currency = Thing.objects.get_default()
+        bankAccount = Invoice.get_my_company_info().bankaccount
+        
+        processCredit(companyInfo, int(options['value']), 
+                      currency, bankAccount)
