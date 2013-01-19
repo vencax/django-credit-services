d470400ba8483da7c6bfc8b59fe65101996a2859
Vaclav Klecanda <vencax77@gmail.com>
2013-01-19 19:04:47 +0100

    autocompilation locale string during setup

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
+import os
+import subprocess
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 
-print find_packages()
+
+class MyInstall(install):
+    def run(self):
+        projpath = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                'creditservices')
+        print 'Generating MO files in %s' % projpath
+        subprocess.call(['django-admin.py', 'compilemessages'],
+                        cwd=projpath)
+        install.run(self)
 
 setup(
     name='django-credit-services',
@@ -8,7 +19,8 @@ setup(
     description='Allows users to prepay some services.',
     author='Vaclav Klecanda',
     author_email='vencax77@gmail.com',
-    url='https://github.com/applecat/django-simple-poll',
+    url='www.vxk.cz',
     packages=find_packages(),
     include_package_data=True,
+    cmdclass={'install': MyInstall},
 )
