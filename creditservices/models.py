07bf436d1188700e5e1caf7f96f766dd9914f05f
Vaclav Klecanda <vencax77@gmail.com>
2013-01-20 19:49:07 +0100

    fix: ugettext instead gettext

--- creditservices/models.py
+++ creditservices/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 from invoices.models import CompanyInfo
 from valueladder.models import Thing
 from django.contrib.auth.models import User
