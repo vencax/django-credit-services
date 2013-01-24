b0ed3b05f34d715363b721246db547af70d46b26
Vaclav Klecanda <vencax77@gmail.com>
2013-01-24 18:43:39 +0100

    company user in CreditCHangeRecordAdmin

--- creditservices/admin.py
+++ creditservices/admin.py
@@ -4,13 +4,19 @@ Created on Jun 7, 2012
 @author: vencax
 '''
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 
 from .models import CreditChangeRecord
 
 
+def companyUser(self):
+    return self.company.user
+companyUser.short_description = _('username')
+
+
 class CreditChangeRecordAdmin(admin.ModelAdmin):
-    list_display = ('user', 'change', 'currency', 'increase', 'date')
+    list_display = (companyUser, 'change', 'currency', 'increase', 'date')
     list_filter = ('currency', 'increase')
-    search_fields = ('user__username', 'change')
+    search_fields = ('company__user__username', 'change', 'company__phone')
 
 admin.site.register(CreditChangeRecord, CreditChangeRecordAdmin)
