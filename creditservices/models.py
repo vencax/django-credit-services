b951796bef48e7c907d08e62a5e656e42fcb6ea8
Vaclav Klecanda <vencax77@gmail.com>
2013-06-23 09:32:49 +0200

    removed unnecessary stuff

--- creditservices/models.py
+++ creditservices/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from invoices.models import CompanyInfo
 from valueladder.models import Thing
-from django.contrib.auth.models import User
+
 from invoices.signals import account_change
 from .signals import on_account_change
 
@@ -10,7 +10,6 @@ from .signals import on_account_change
 class CreditChangeRecord(models.Model):
     """
     Record how a credit changes in time.
-    NOTE: it is connected directly to User model. NOT to CompanyInfo!!
     """
     class Meta:
         verbose_name = _('credit change record')
@@ -20,8 +19,6 @@ class CreditChangeRecord(models.Model):
     change = models.FloatField(_('change'))
     increase = models.BooleanField(_('increase'))
     currency = models.ForeignKey(Thing, verbose_name=_('currency'))
-    user = models.ForeignKey(User, related_name='_changeRecords', null=True)
-    """ This is gonna be removed """
     company = models.ForeignKey(CompanyInfo, related_name='changeRecords')
     date = models.DateField(verbose_name=_('date'), editable=False,
                             auto_now_add=True)
